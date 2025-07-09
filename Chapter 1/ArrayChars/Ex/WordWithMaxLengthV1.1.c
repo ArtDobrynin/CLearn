7622251e1babf629c6ddc0d62f90f777a92f476f
artemDobrynin <adobrynin@tangl.cloud>
2025-07-09 12:51:35 +0300

    Refactor directory structure

---
+++ Chapter 1/ArrayChars/Ex/WordWithMaxLengthV1.1.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#define MAXLINE 10
+
+int getFullline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+void main ()
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max = 0;
+    while ((len = getFullline(line, MAXLINE)) > 0)
+        if (len > max)
+        {
+            max = len;
+            copy(longest, line);
+        }
+
+    if (max > 0)
+        printf("%s", longest);
+}
+
+int getFullline(char s[], int lim)
+{
+    int c, i;
+
+    for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
+    {
+        if (i > lim - 1)
+        {
+            break;
+        }
+        s[i] = c;
+    }
+    
+    if (c == '\n')
+    {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy (char to[], char from[])
+{
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
